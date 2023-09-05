dae29830253cb7d07082d7373a58b8da59ac6982
NoFun <hpham15@ncstudents.niagaracollege.ca>
2023-09-05 18:31:08 -0400

    new production build

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,7 +3,6 @@ import App from "../layout/App";
 import RequireAuth from "./RequireAuth";
 import CheckoutWrapper from "../../features/checkout/CheckoutWrapper";
 import Orders from "../../features/orders/OrdersPage";
-import { ContactPage, Inventory, Login } from "@mui/icons-material";
 import Register from "../../features/account/Register";
 import Catalog from "../../features/catalog/CatalogPage";
 import ProductDetails from "../../features/catalog/ProductDetails";
@@ -12,6 +11,9 @@ import NotFound from "../errors/NotFound";
 import ServerError from "../errors/ServerError";
 import AboutPage from "../../features/about/AboutPage";
 import BasketPage from "../../features/basket/BasketPage";
+import Inventory from "../../features/admin/Inventory";
+import ContactPage from "../../features/contact/ContactPage";
+import Login from "../../features/account/Login";
 
 export const router = createBrowserRouter([
     {
