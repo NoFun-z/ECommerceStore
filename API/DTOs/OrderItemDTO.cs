4825c762dee0ab09195c28aae81e08c7a3ad4261
NoFun <hpham15@ncstudents.niagaracollege.ca>
2023-09-26 22:47:27 -0400

    finalver order detail NaN fixed

--- API/DTOs/OrderItemDTO.cs
+++ API/DTOs/OrderItemDTO.cs
@@ -8,5 +8,6 @@ namespace API.DTOs
         public string PictureURL { get; set; }
         public long Price { get; set; }
         public int Quantity { get; set; }
+        public long Discount {get; set;}
     }
 }
